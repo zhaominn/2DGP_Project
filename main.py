e4422ea2c4c555f910eb022c45d62ab5b4ffe535
zhaominn <somin207@naver.com>
2024-10-15 12:47:05 +0900

    기본 화면 띄우기

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ root = tkinter.Tk()
 monitor_height = root.winfo_screenheight()
 monitor_width = root.winfo_screenwidth()
 
-running = True
-open_canvas(monitor_width,monitor_height)
+class Background:
+    stage_num=None #0 시작 화면 1 기본 화면
+    def __init__(self):
+        self.x, self.y = monitor_width/2, monitor_height/2
+        self.image = load_image('image//background//spring_ground.png')
+        Background.stage_num=1
+
+    def draw(self):
+        self.image.clip_draw(0,0,monitor_width,monitor_height,self.x, self.y)
+
+    def update(self):
+        pass
 
 def handle_events():
     global running
@@ -19,8 +29,35 @@ def handle_events():
         else:
             pass
 
+def reset_world():
+    global running
+    global world
+    global background
+
+    running = True
+    world = []
+
+    background = Background()
+    world.append(background)
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+open_canvas(monitor_width,monitor_height)
+reset_world()
+
 while running:
     handle_events()
+    update_world()
+    render_world()
     delay(0.01)
 
 close_canvas()
