f06c53b8be4e5b0e8034fbf34d5c4a5525c34ae1
zhaominn <somin207@naver.com>
2024-10-15 12:29:37 +0900

    esc 키로 창 닫기

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ root = tkinter.Tk()
 monitor_height = root.winfo_screenheight()
 monitor_width = root.winfo_screenwidth()
 
+running = True
 open_canvas(monitor_width,monitor_height)
 
+def handle_events():
+    global running
 
-while 1:
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            pass
+
+while running:
+    handle_events()
     delay(0.01)
 
 close_canvas()
