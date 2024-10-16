5cb1eaae3195d41eb48c3bbe382846f581f329d6
zhaominn <somin207@naver.com>
2024-10-16 22:47:26 +0900

    스테이지 0 enter 누르면 게임 시작 구현

--- main.py
+++ main.py
@@ -10,15 +10,27 @@ class Background:
     stage_num=None #0 시작 화면 1 기본 화면
     def __init__(self):
         self.x, self.y = monitor_width/2, monitor_height/2
-        Background.stage_num = 1
+        self.stage_num = 0
 
-        self.background_image = load_image('image//stage1_background//spring_ground.png')
+        # 이미지 로딩
+        self.background0_image = load_image('image//stage0_start//start_image.png')
+
+        self.background1_image = load_image('image//stage1_background//spring_ground.png')
         self.farm_image =load_image('image///stage1_background/farm.png')
         self.greenhouse_image = load_image('image///stage1_background/greenhouse.png')
         self.house_image = load_image('image///stage1_background/house.png')
 
     def draw(self):
-        self.background_image.clip_draw(0,0,monitor_width,monitor_height,self.x, self.y)
+        if self.stage_num == 0:
+            self.draw_start_screen()
+        elif self.stage_num == 1:
+            self.draw_main_screen()
+
+    def draw_start_screen(self):
+        self.background0_image.clip_draw(0, 0, monitor_width, monitor_height, self.x, self.y)
+
+    def draw_main_screen(self):
+        self.background1_image.clip_draw(0, 0, monitor_width, monitor_height, self.x, self.y)
         self.farm_image.clip_draw(0, 0, 281, 279, 300, 550)
         self.greenhouse_image.clip_draw(0, 0, 280, 400, 1300, 600)
         self.house_image.clip_draw(0, 0, 363, 361, 900, 650)
@@ -33,6 +45,8 @@ def handle_events():
     for event in events:
         if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.key == SDLK_RETURN and background.stage_num == 0:
+            background.stage_num = 1
         else:
             pass
 
