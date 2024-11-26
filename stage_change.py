7085cfe5ad6f8019a72c4f7893db1fe61bccec7f
zhaominn <somin207@naver.com>
2024-11-27 01:36:00 +0900

    장면 전환 애니메이션 이미지 하나 없앰(눈아파서)

--- stage_change.py
+++ stage_change.py
@@ -4,7 +4,7 @@ import game_framework
 
 class Change_stage:
     def __init__(self):
-        self.images = [load_image('image//change_stage//stage_change_%d.png' % x) for x in range(10 + 1)]
+        self.images = [load_image('image//change_stage//stage_change_%d.png' % x) for x in range(10)]
 
     def change_stage(self, next_mode):
         for image in self.images:
@@ -13,7 +13,7 @@ class Change_stage:
         game_framework.change_mode(next_mode)
 
     def start_stage(self, now_ground):
-        for i in range(10,-1,-1):
+        for i in range(9,-1,-1):
             now_ground.draw()
             self.images[i].draw_now(800,400)
             delay(0.1)
