84f3181960808f21065629d44f4aed3bc55a94c4
zhaominn <somin207@naver.com>
2024-12-02 21:03:21 +0900

    장면 전환 사운드 추가

--- stage_change.py
+++ stage_change.py
@@ -5,10 +5,14 @@ import game_framework
 class Change_stage:
     def __init__(self):
         self.images = [load_image('image//animation//change_stage//stage_change_%d.png' % x) for x in range(10)]
+        self.sound=load_wav('sound//change_stage.wav')
+        self.sound.set_volume(30)
+
 
     def change_stage(self, next_mode):
         for image in self.images:
             image.draw_now(800,400)
+            self.sound.play()
             delay(0.1)
         game_framework.change_mode(next_mode)
 
@@ -16,5 +20,6 @@ class Change_stage:
         for i in range(9,-1,-1):
             now_ground.draw()
             self.images[i].draw_now(800,400)
+            self.sound.play()
             delay(0.1)
 
